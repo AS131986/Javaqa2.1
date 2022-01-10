36af00e88400cf55433d2fdc4473eabec51fa767
Alexander <AlexanderSGSklyar@mail.ru>
2022-01-10 20:44:08 +0300

    First

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int priceTicket = 500;
+        int bonusOneMile = 20;
+        int bonusMile = priceTicket / bonusOneMile;
+        System.out.println("Начислено бонусов: " + bonusMile);
+    }
+}
